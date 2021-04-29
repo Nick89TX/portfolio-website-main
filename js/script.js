726c9cef31fb3a48ddb0b48e4df970c6887622a9
Nick89TX <naleksee89rus@gmail.com>
2021-04-29 19:25:08 -0400

    2.3 fixing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-console.log("Hello world");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
